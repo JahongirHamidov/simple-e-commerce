2afe37f276156c357b3f19dcfe081485a646d2c8
Jahongir <hamidovjahongir444@gmail.com>
2020-10-11 10:39:14 +0500

    .

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -2,13 +2,16 @@ import React from 'react'
 import {data} from '../../data'
 import {GlobalState} from '../../globalState'
 import {MyCard} from '../Card/MyCard'
+import {Ad} from '../Ad/Ad.js'
+
 export function Search() {
 const {value,setValue} = React.useContext(GlobalState)
 
     return (
         <div>
+            <Ad />
             <MyCard state = {data.products.map((e)=>{
-                if((e.name==value) === true)    
+                if(e.name == value)    
                 return value
                 })}/>      
         </div>
